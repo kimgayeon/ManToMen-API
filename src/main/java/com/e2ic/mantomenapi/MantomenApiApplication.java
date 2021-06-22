ae2d23019a2179e9bc5acdc2325dcdb810a1ed3b
gayeon <gimgayeon@gimgayeon-ui-MacBookPro.local>
2021-06-22 16:17:56 +0900

    init

---
+++ src/main/java/com/e2ic/mantomenapi/MantomenApiApplication.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomenapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MantomenApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MantomenApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/e2ic/mantomenapi/MantomenApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2ic.mantomenapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MantomenApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
